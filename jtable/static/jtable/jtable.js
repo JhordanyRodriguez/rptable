2bfcb931c86568e66235b0a864979f24413d338f
Jhordany Rodriguez Parra <jhoropa19@gmail.com>
2022-04-10 17:23:01 +0930

    Add basic table visualisation

--- jtable/static/jtable/jtable.js
+++ jtable/static/jtable/jtable.js
@@ -1,2 +1,60 @@
-alert('hola amigos, making a change here ');
-console.log(reserve_animals);
+//https://www.w3schools.com/jsref/dom_obj_table.asp
+
+let JTable = class{
+    /**
+     * Returns a table
+     * @param {string} html_parent :string the name of the html object the table will be attached to.
+     * @param {string} name name
+     */
+    constructor(html_parent, name){
+        let parent = document.getElementById(html_parent);
+        this.rows_per_page =60;
+        if (parent== undefined){
+            alert("could not find the parent to create the table");
+        } 
+        else
+        {
+            this.my_html = document.createElement('TABLE');
+            parent.appendChild(this.my_html);
+            this.my_html.id = 0;
+            this.column_headers = [];
+            
+        }
+    };
+
+    /**
+     * Creates the html structure for the given data
+     * maps every field of the first element in the list data
+     * to a column, then displays the columns for each element
+     * in the list data.
+     * @param {json} data 
+     */
+    fill_data(data)
+    {
+        this.data = data;
+        this.column_names = Object.keys(this.data[0]);
+        console.log(this.column_names);
+        let header_table = this.my_html.insertRow(0);
+        for (let i =0; i < this.column_names.length; i ++){
+            let td = header_table.insertCell();
+            td.id = this.name +"__header__"+this.column_names;
+            td.innerHTML = this.column_names[i];            
+        }
+        // now we fill the data 
+        for (let i =0; i < this.data.length; i ++)
+        {
+            let data_row = this.my_html.insertRow();
+            for (let f =0; f < this.column_names.length; f++)
+            {
+                let td = data_row.insertCell();
+                td.innerHTML = this.data[i][this.column_names[f]];
+            }
+        }
+
+    }
+
+    show(){
+        this.my_html.display='block';
+    }
+};
+
---
+++ jtable/static/jtable/main.js
@@ -0,0 +1,4 @@
+let my_table = new JTable("table_container", "cosa");
+my_table.fill_data(reserve_animals);
+
+console.log(reserve_animals);
