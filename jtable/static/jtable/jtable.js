aee6a75c9f363ebd6cc30b464e18cc218b8f8a00
Jhordany Rodriguez Parra <jhoropa19@gmail.com>
2022-04-15 16:42:40 +0930

    Add filters row

--- jtable/static/jtable/jtable.js
+++ jtable/static/jtable/jtable.js
@@ -19,23 +19,32 @@ let JTable = class{
             this.my_html.id = 0;
             this.column_headers = [];
             this.my_html.classList.add('jtable_table');
+            this.header = null;
         }
     };
 
     /**
-     * Creates the html structure for the given data
+     * sets the data that will be displayed by this instance.
+     * @param {list[json]} data 
+     */
+    set_data(data){
+        this.data = data;
+    }
+
+    /**
+     * Creates the html structure of the headers to display the data.
      * maps every field of the first element in the list data
      * to a column, then displays the columns for each element
      * in the list data.
-     * @param {json} data 
+     * populates attributes: column_names, header
+     * @param {list[json]} data 
      */
-    fill_data(data)
-    {
-        this.data = data;
+    create_headers()
+    {        
         this.column_names = Object.keys(this.data[0]);
         console.log(this.column_names);
-        var header = this.my_html.createTHead();
-        let header_row = header.insertRow(0);
+        this.header = this.my_html.createTHead();
+        let header_row = this.header.insertRow(0);
         
         for (let i =0; i < this.column_names.length; i ++){
             var th = document.createElement('th');
@@ -43,10 +52,49 @@ let JTable = class{
             th.innerHTML = this.column_names[i];            
             //let td = header_row.insertCell();
             th.id = this.name +"__header__"+this.column_names;
-            header_row.appendChild(th);
-            //td.classList.add('jtable_header');
-            //td.innerHTML = this.column_names[i];            
+            header_row.appendChild(th);          
         }
+    }
+
+    /**
+     * set the top of the filter based on how much space is being consumed
+     * by the headers. At the moment we have 
+     * top_filters = 2* height_headers_rect + 5 px
+     */
+    set_filter_top(){
+        // we get the bottom of the headers row
+        let headers = document.getElementsByClassName("jtable_header");
+        if (headers != undefined && headers.length > 0){
+            let first_header_bottom = headers[0].getBoundingClientRect().bottom;
+            let first_header_top = headers[0].getBoundingClientRect().top;
+            let new_top = (first_header_bottom - first_header_top)*2 +5;
+            let filters = document.getElementsByClassName("jtable_filter");
+            if (filters != undefined){
+                for (let i=0; i < filters.length; i ++){
+                    console.log('adjusting filters lenght '+ first_header_bottom);
+                    filters[i].style.top = new_top+ "px";
+                }
+            }
+        }
+    }
+
+    create_filters()
+    {
+        
+        
+        this.filter_row =this.header.insertRow(1);
+        console.log('inserted row filter');
+        for (let i =0; i < this.column_names.length; i ++)
+        {
+            var th = document.createElement('th');
+            th.classList.add('jtable_filter');
+            th.innerHTML = this.column_names[i];            
+            th.id = this.name +"__filter__"+this.column_names;
+            this.filter_row.appendChild(th);
+        }
+    }
+
+    fill_data(){
         // now we fill the data 
         for (let i =0; i < this.data.length; i ++)
         {
@@ -58,8 +106,6 @@ let JTable = class{
             }
         }
         this.my_top = my_table.my_html.getBoundingClientRect().top;
-        
-
     }
 
     show(){
--- jtable/static/jtable/main.js
+++ jtable/static/jtable/main.js
@@ -1,3 +1,6 @@
 var my_table = new JTable("table_container", "cosa");
-my_table.fill_data(reserve_animals);
+my_table.set_data(reserve_animals);
+my_table.create_headers();
+my_table.create_filters(); 
+my_table.fill_data();
 console.log(reserve_animals);
